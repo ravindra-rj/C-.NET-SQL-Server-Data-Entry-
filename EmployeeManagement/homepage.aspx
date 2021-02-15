5163740ecc305e7d473167c3f2a914255564bf73
Sunny-Jadaun <vikramsinghjadaun1801@gmail.com>
2021-02-15 07:24:24 +0530

    wrapping up project

---
+++ EmployeeManagement/homepage.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="homepage.aspx.cs" Inherits="EmployeeManagement.homepage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div >
+            <table align="center">
+                <tr>
+                    <td>UserID</td>
+                    <td>
+                        <asp:TextBox ID="txtUserID" runat="server" Width="258px"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td>
+                        <asp:TextBox ID="txtPass" runat="server" Width="255px"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Role</td>
+                    <td>
+                        <asp:TextBox ID="txtRole" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        &nbsp;<asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" /></td>
+                    <td>
+                        &nbsp;</td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EmployeeManagement/homepage.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeeManagement
+{
+    public partial class homepage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dataemp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO [dbo].[Table] VALUES ('"+txtUserID.Text+"','"+txtPass.Text+"','"+txtRole.Text+"')",con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Response.Write("Data Inserted successfully");
+            con.Close();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=emplmanage;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand(@"UPDATE [dbo].[Table] SET[userid] = '" + txtUserID.Text + "',[password]='"+txtPass.Text+"',[role]='"+txtRole.Text+"'", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Response.Write("Data Updated");
+            con.Close();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand(@"DELETE FROM [dbo].[Table] WHERE [userid] = '"+txtUserID.Text+"'", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Response.Write("Data Deleted successfully");
+            con.Close();
+        }
+
+    }
+    }
